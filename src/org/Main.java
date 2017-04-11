e2f9d13b0e8a6d0ad456d6214111ed82563a2357
Evan Duffy <eduffy2015@spyponders.com>
2017-04-10 20:55:36 -0400

    Instantiated Database Controller and Interface in Main

--- src/org/Main.java
+++ src/org/Main.java
@@ -2,18 +2,26 @@ package org;
 
 import static javafx.application.Application.launch;
 
+import Database.DatabaseController;
+import Database.DatabaseEditor;
+import Database.DatabaseInterface;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-      CentralController cc = new CentralController();
-      cc.startUI(primaryStage);
-    }
+  DatabaseController dbc = new DatabaseController("org.apache.derby.jdbc.EmbeddedDriver",
+      "jdbc:derby:testDB;create=true");
+  DatabaseInterface dbe = new DatabaseEditor(dbc);
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    CentralController cc = new CentralController();
+    cc.startUI(primaryStage);
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
